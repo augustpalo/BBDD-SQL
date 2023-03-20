5ded1090557849d66bbbd47ccd50261303f359e7
IESCJ <august.horacio.paloluoma@iesciudadjardin.com>
2023-03-20 13:10:19 +0100

    pr14 ej11

--- consultas.sql
+++ consultas.sql
@@ -80,6 +80,103 @@ group by numde; */
 --    con igual comisión. .. note:
 --La edad dependerá de la fecha en la que realicemos la consulta
 
-select comis, round(avg(salar),2), round(avg((SYSDATE-fecna)/365),1)
+/* select comis, round(avg(salar),2), round(avg((SYSDATE-fecna)/365,1))
 from empleados
 group by comis;
+ */
+
+/* 4. Repite la consulta anterior expresando la edad en años cumplidos. (Aunque en este caso se obtiene lo mismo, la
+edad media podría variar de una consulta a otra dependiendo del momento en el que se realice la consulta). */
+
+/* select comis, round(avg(salar),2), trunc(avg((SYSDATE-fecna)/365))
+from empleados
+group by comis;
+ */
+
+-- Practica 13
+
+-- 5. Hallar el maximo de la suma de los salarios de todos los departamentos
+
+/* select numde, sum(salar)
+from empleados
+group by numde
+HAVING sum(salar) >= ALL
+                    (select sum(salar)
+                    from empleados
+                    group by numde); */
+
+
+-- Practica 14
+
+/* 1.  Para cada departamento con presupuesto inferior a 35.000 C, hallar le nombre del Centro donde está ubicado
+y el máximo salario de sus empleados (si dicho máximo excede de 1.500 C). Clasificar alfabéticamente por
+nombre de departamento. */ 
+
+/* 2. Hallar por orden alfabético los nombres de los departamentos que dependen de los que tienen un presupuesto
+inferior a 30.000 C. También queremos conocer el nombre del departamento del que dependen y su presupuesto.
+ */
+
+/* select D1.nomde as "Dept.", D2.nomde as "Dept. del que depende", D2.presu as "Presu."
+from departamentos D1 JOIN departamentos D2 ON D1.depde = D2.numde
+WHERE D1.depde in (select numde from departamentos where presu < 30)
+order by 1;
+ */
+/* 3. Obtener los nombres y los salarios medios de los departamentos cuyo salario medio supera al salario medio de
+la empresa.
+ */
+
+/*  select nomde, avg(salar) as "Salario medio"
+ from departamentos D join empleados E on E.numde = D.numde
+ group by nomde
+ having avg(E.salar) > (SELECT avg(salar) from empleados);
+ */
+
+/*5. Para los departamentos cuyo presupuesto anual supera los 35.000 C, hallar cuantos empleados hay por cada
+extensión telefónica.*/
+
+/* select nomde, extel, count(numem)
+from departamentos D join empleados E on D.numde = E.numde
+where presu>35
+group by extel, nomde; */
+
+/* 6. Hallar por orden alfabético los nombres de los empleados y su número de hijos para aquellos que son directores
+en funciones. */
+
+/* select nomem, NUMHIL
+from EMPLEADOS E join departamentos D on E.numde = D.numde
+where numem in (select direc from departamentos where tidir = 'F')
+order by nomem; */
+
+/* 7. Hallar si hay algún departamento (suponemos que sería de reciente creación) que aún no tenga empleados
+asignados ni director en propiedad.
+ */
+
+/* select nomde
+from departamentos D join empleados E on D.numde = E.numde
+group by nomde
+having count(e.numem) = 0;
+ */
+--mas correcto:
+/* select nomde
+from departamentos
+where numde NOT IN (select numde FROM EMPLEADOS); */
+
+-- muestra los departamentos con el numero de empleado --
+
+/* select nomde, D.numde, count(numem)
+from departamentos D join empleados E on D.numde = E.numde
+group by nomde, D.numde
+order by count(numem);
+ */
+-- 10. Muestra los departamentos que no tienen empleados.
+
+select nomde
+from departamentos
+where numde not in (select numde from empleados);
+
+/*11. Muestra los nombres de departamentos que no tienen empleados haciendo uso la combinación externa LEFT
+JOIN. Muestra una segunda columna con los nombres de empleados para asegurarnos que realmente esta a
+NULL. */
+
+select nomde, nomem
+from departamentos D LEFT JOIN 
