1f871557c475a8ff28e74f92c79ab501fd49fa42
AugustP <augustpaloluoma@gmail.com>
2023-04-16 13:46:02 +0200

    552 pra2

---
+++ 552/practica 2/inserciones.sql
@@ -0,0 +1,21 @@
+-- Inserta dos directores:
+INSERT INTO DIRECTOR VALUES ('Pedro Martin', 'España');
+INSERT INTO DIRECTOR VALUES ('Pierre Martin', 'Francia');
+
+-- Inserta 4 películas. Todas ellas están dirigidas por alguno de los directores anteriores:
+INSERT INTO PELICULA VALUES(1, 'Señor de los arillos', '2nd century fox', 'España', '12/4/2000', 'Pedro Martin');
+
+-- Cambia la nacionalidad para los directores. Por ejemplo de “Estadounidense” a “USA” o similar,
+-- dependiendo de los valores que hayas introducido.
+
+UPDATE DIRECTOR SET NACIONALIDAD = 'ESP' WHERE NACIONALIDAD = 'España';
+
+-- Modifica los datos de todos los socios para que el avalista sea un único socio, siempre el mismo
+-- para todos, excepto para el avalista mismo que no dispone de ninguno.
+
+UPDATE SOCIO SET AVALADOR = '12345678A' WHERE DNI != '12345678A';
+
+-- Elimina los socios cuyo número de teléfono empiece por una cifra inferior a 5. ¿Qué sucede?¿Por
+-- qué?
+
+DELETE 
