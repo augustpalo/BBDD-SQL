8448f956647da53b330c1275a85763b7ad22bed6
August Paloluoma <august.horacio.paloluoma@iesciudadjardin.es>
2023-05-15 12:52:20 +0200

    15052023

--- pr5.sql
+++ pr5.sql
@@ -4,14 +4,14 @@ nombre, salario anterior y final. Utiliza un cursor explícito. La transacción 
 medias. Si por cualquier razón no es posible actualizar todos estos salarios, debe deshacerse el
 trabajo a la situación inicial. */
 
-create or replace procudure subir_salario as
+/* create or replace procudure subir_salario as
     cursor c is
         select numem, nomem, salar, rowid
         from EMPLEADOS WHERE numhi >= 2 & salar < 2000;
         salar_nuevo number;
     begin
       for registro in c loop
-        update EMPLEADOS et SALAR=SALAR*1.1 
+        update EMPLEADOS set SALAR=SALAR*1.1 
         where rowid = registro.rowid;
         salar_nuevo := registro.salar*1.1;
         
@@ -21,8 +21,8 @@ create or replace procudure subir_salario as
         dbms_output.put_line(registro.numem || '' || registro.nomem || ' : ' || registro.salar || '--> ' || salar_nuevo);     
       end loop
     end;
-    /
-    
+    / */
+
 /* Escribe un procedimiento que reciba dos parámetros (número de departamento, hijos). Deberá
 crearse un cursor explícito al que se le pasarán estos parámetros y que mostrará los datos de
 los empleados que pertenezcan al departamento y con el número de hijos indicados. Al final se
@@ -42,4 +42,35 @@ EMPLEADOS_DEPT_HIJ(DEPT INTEGER, HIJ INTEGER) AS
   END LOOP
   dbms_output.put_line('Empleados obtenidos: ' || CONT);
 END;
+/
+
+/* Escribe un procedimiento con un parámetro para el nombre de empleado, que nos muestre la edad
+de dicho empleado en años, meses y días */
+
+Gestión de Bases de Datos, Versión 1.0
+-- 4. Escribe un procedimiento con un parámetro para el nombre de empleado,
+-- que nos muestre la edad de dicho empleado en años, meses y días.
+
+CREATE OR REPLACE
+PROCEDURE Edad_Empleado (nombre EMPLEADOS.NOMEM%TYPE) AS
+-- Utilizamos un cursor explícito por si existiese más de un empleado
+-- con el mismo nombre.
+  CURSOR c(nom EMPLEADOS.NOMEM%TYPE) IS
+    SELECT NOMEM, FECNA
+    FROM EMPLEADOS WHERE NOMEM = nom;
+  meses NUMBER;
+  a NUMBER;
+  m NUMBER;
+  d NUMBER;
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('EMPLEADO: AÑOS MESES DÍAS');
+  FOR registro IN c(nombre) LOOP
+    meses := MONTHS_BETWEEN (SYSDATE, registro.FECNA);
+    a := meses/12;
+    m := MOD (meses, 12);
+    d := (m - TRUNC (m))*30;-- parte decimal de m multiplicada por 30
+    DBMS_OUTPUT.PUT_LINE(registro.NOMEM || ': '
+    || TRUNC(a) || ' ' || TRUNC(m) || ' ' || TRUNC(d) );
+  END LOOP;
+END Edad_Empleado;
 /
